33b97f764d0f804bbc0dc6a41f7eaf1d89d6ed91
JEDIDIAH YSAMM CAYCHINGCO (SHP-IRIS-ISD-OOCLL/MNL) <CAYCHJE@OOCL.COM>
2019-10-08 19:44:53 +0800

    Create unaligned multiplication table

--- __tests__/main-spec.js
+++ __tests__/main-spec.js
@@ -25,3 +25,91 @@ it ('should be false if not within two and one thousand inclusive', () => {
 	const result = output.withinTwoAndOneThousand(input);
    expect(result).toBe(false);
 });
+
+it ('should create single multiplication single digit', () => {
+	const currentRow=2;
+	const currentColumn=3;
+	const result = output.getSingleMultiplication(currentRow, currentColumn);
+   expect(result).toBe("2*3=6");
+});
+
+it ('should create single multiplication double digit', () => {
+	const currentRow=40;
+	const currentColumn=20;
+	const result = output.getSingleMultiplication(currentRow, currentColumn);
+   expect(result).toBe("40*20=800");
+});
+
+it ('should create a multiplication 3 items', () => {
+	const currentRow=2;
+	const currentColumn=4;
+	const result = output.getMultiplicationRow(currentRow, currentColumn);
+   expect(result).toBe("2*4=8 3*4=12 4*4=16 ");
+});
+
+it ('should create a multiplication row 5 items', () => {
+	const currentRow=6;
+	const currentColumn=10;
+	const result = output.getMultiplicationRow(currentRow, currentColumn);
+   expect(result).toBe("6*10=60 7*10=70 8*10=80 9*10=90 10*10=100 ");
+});
+
+it ('should create a multiplication same start and end', () => {
+	const start=2;
+	const end=2;
+	const result = output.getMultiplicationTable(start, end);
+   expect(result).toBe("2*2=4 \n");
+});
+
+it ('should create a multiplication with the end = start+1', () => {
+	const start=2;
+	const end=3;
+	const result = output.getMultiplicationTable(start, end);
+	const expectedOutput =
+		"2*2=4 \n"+
+		"2*3=6 3*3=9 \n";
+   expect(result).toBe(expectedOutput);
+});
+
+it ('should create a multiplication with the end = start+2', () => {
+	const start=2;
+	const end=4;
+	const result = output.getMultiplicationTable(start, end);
+	const expectedOutput =
+		"2*2=4 \n"+
+		"2*3=6 3*3=9 \n"+
+		"2*4=8 3*4=12 4*4=16 \n";
+   expect(result).toBe(expectedOutput);
+});
+
+it ('should create a multiplication with the end = start+2. 2 digit input', () => {
+	const start=10;
+	const end=12;
+	const result = output.getMultiplicationTable(start, end);
+	const expectedOutput =
+		"10*10=100 \n"+
+		"10*11=110 11*11=121 \n"+
+		"10*12=120 11*12=132 12*12=144 \n";
+   expect(result).toBe(expectedOutput);
+});
+
+it ('should return null if start is greater than end', () => {
+	const start=10;
+	const end=2;
+	const result = output.getMultiplicationTable(start, end);
+   expect(result).toBe(null);
+});
+
+it ('should return null if an start is one', () => {
+	const start=10;
+	const end=1;
+	const result = output.getMultiplicationTable(start, end);
+   expect(result).toBe(null);
+});
+
+it ('should return null if an start is one thousand one', () => {
+	const start=1001;
+	const end=1337;
+	const result = output.getMultiplicationTable(start, end);
+   expect(result).toBe(null);
+});
--- main.js
+++ main.js
@@ -1,13 +1,46 @@
 "use strict";
+
+function getMultiplicationTable(start, inclusiveEnd){
+	if(!startIsNotGreaterThanEnd(start, inclusiveEnd)){
+		return null;
+	}
+
+	if(!withinTwoAndOneThousand(start) || !withinTwoAndOneThousand(inclusiveEnd)){
+		return null;
+	}
+
+	let multiplicationTable = "";
+	for(let currentColumn = start; currentColumn<=inclusiveEnd; currentColumn++){
+		multiplicationTable+=getMultiplicationRow(start, currentColumn);
+		multiplicationTable+="\n";
+	}
+	return multiplicationTable;
+}
+
 function startIsNotGreaterThanEnd (start, end) {
-    return start <=end;
+   return start <=end;
 }
 
 function withinTwoAndOneThousand(input){
 	return input >=2 && input <=1000;
 }
 
+function getMultiplicationRow(inclusiveStart, inclusiveEnd){
+	let multiplicationRow = "";
+	for(let current = inclusiveStart; current<=inclusiveEnd; current++){
+		multiplicationRow+=getSingleMultiplication(current, inclusiveEnd)+" ";
+	}
+	return multiplicationRow;
+}
+
+function getSingleMultiplication(currentRow, currentColumn){
+	return `${currentRow}*${currentColumn}=${currentRow*currentColumn}`;
+}
+
 module.exports = {
 	startIsNotGreaterThanEnd: startIsNotGreaterThanEnd,
-	withinTwoAndOneThousand: withinTwoAndOneThousand
+	withinTwoAndOneThousand: withinTwoAndOneThousand,
+	getSingleMultiplication: getSingleMultiplication,
+	getMultiplicationRow: getMultiplicationRow,
+	getMultiplicationTable: getMultiplicationTable
 };
