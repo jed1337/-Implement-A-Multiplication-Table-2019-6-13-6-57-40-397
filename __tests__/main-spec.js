dac4d6797669d5a86ab6c64f6bc66d09fa5624ac
JEDIDIAH YSAMM CAYCHINGCO (SHP-IRIS-ISD-OOCLL/MNL) <CAYCHJE@OOCL.COM>
2019-10-08 18:15:58 +0800

    Added within two and one thousand function and tests

--- __tests__/main-spec.js
+++ __tests__/main-spec.js
@@ -1,15 +1,27 @@
-const startIsNotGreaterThanEnd = require('../main');
+const output= require('../main');
 
 it ('should be true that start is not greater than end', () => {
 	const start = 2;
 	const end = 3;
-	const result = startIsNotGreaterThanEnd(start, end);
+	const result = output.startIsNotGreaterThanEnd(start, end);
    expect(result).toBe(true);
 });
 
 it ('should be false that start is greater than end', () => {
 	const start = 22;
 	const end = 3;
-	const result = startIsNotGreaterThanEnd(start, end);
+	const result = output.startIsNotGreaterThanEnd(start, end);
+   expect(result).toBe(false);
+});
+
+it ('should be true if between two and one thousand inclusive', () => {
+	const input = 420;
+	const result = output.withinTwoAndOneThousand(input);
+   expect(result).toBe(true);
+});
+
+it ('should be false if not within two and one thousand inclusive', () => {
+	const input = 1337;
+	const result = output.withinTwoAndOneThousand(input);
    expect(result).toBe(false);
 });
--- main.js
+++ main.js
@@ -1,5 +1,13 @@
+"use strict";
 function startIsNotGreaterThanEnd (start, end) {
     return start <=end;
 }
 
-module.exports = startIsNotGreaterThanEnd;
+function withinTwoAndOneThousand(input){
+	return input >=2 && input <=1000;
+}
+
+module.exports = {
+	startIsNotGreaterThanEnd: startIsNotGreaterThanEnd,
+	withinTwoAndOneThousand: withinTwoAndOneThousand
+};
